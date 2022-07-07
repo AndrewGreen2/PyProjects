01769768ec7b01101b3dfb2573623df8eb7f6621
Andrew <atiddlytortoise@gmail.com>
2022-07-07 15:49:37 +1000

    checks for duplicate guesses

--- hangman.py
+++ hangman.py
@@ -5,11 +5,11 @@ word = words[random.randrange(0,3)]
 word_state = []
 wrong_guesses = []
 
-gallows1 = "   /----\\=========\n"
+gallows1 = "   /----\\=========>\n"
 gallows2 = "   |    |        |\n"
 gallows3 = "   |    |  \n"
 gallows4 = "___|    |___\n"
-gallows5 = "/ /      \\ \\"
+gallows5 = "/ /      \\ \\___________"
 head = "   (o _ o)  \n"
 deadhead = "   (x _ x)  \n"
 body = " /(       )\\ \n"
@@ -22,7 +22,7 @@ def print_word_state():
     currentState = ""
     for i in word_state:
         currentState = currentState + i
-    print(currentState)
+    print("\n", currentState)
     
 def print_hangman(): 
     hangman = "" 
@@ -41,26 +41,37 @@ def print_hangman():
     if len(wrong_guesses)>6:
         print("Game Over!")
         hangman = gallows1 + gallows2 + gallows3[:-1] + deadhead + gallows3[:-1] + body + gallows4[:-1] + legs + gallows5
+        print(hangman)
+        exit()
     print(hangman)
+
+def checkGuess(guess):
+    for i in wrong_guesses:
+        if i == guess:
+            print("You have already guessed this letter!")
+            print_word_state()
+            return False
+    return True
     
 def makeGuess():
     x = 0 
     rightGuess = False    
-    print("Guess a letter: ")
-    guess = input()               
-    for letter in word:
-        x += 1
-        if letter == guess:            
-            word_state[x-1] = guess 
-            rightGuess = True 
-    if rightGuess:
-        print_word_state()
-        return
-    else:
-        wrong_guesses.append(guess)
-        print("Incorrect Letters: ", wrong_guesses)
-        print_hangman()
-        print_word_state()
+    print("\nGuess a letter: ")
+    guess = input()
+    if checkGuess(guess):               
+        for letter in word:
+            x += 1
+            if letter == guess:            
+                word_state[x-1] = guess 
+                rightGuess = True 
+        if rightGuess:
+            print_word_state()
+            return
+        else:
+            wrong_guesses.append(guess)
+            print("Incorrect Letters: ", wrong_guesses, "\n")
+            print_hangman()
+            print_word_state()
 
 print_word_state()
 while len(wrong_guesses) < 10:
