3e36ad733b56f8ad023681c9d08b29d76fcba75d
Andrew <green.andrew343@gmail.com>
2023-04-07 10:29:07 +1000

    error handling

--- weatherApp/weather_app.py
+++ weatherApp/weather_app.py
@@ -7,18 +7,22 @@ app = Flask(__name__)
 @app.route("/", methods=["POST","GET"])
 def index():
     if request.method == "POST":
-        city, temperature = get_temperature()
-        return f"<p>The current temperature in {city} is {temperature}°C.</p>"
+        city = request.form["usr_city"]
+        try:
+            temperature = get_temperature(city)
+            return f"<p>The current temperature in {city} is {temperature}°C.</p>"
+        except KeyError:
+             return f"<p>The current temperature in {city} is unknown."
     else:
         return render_template("index.html")
-    
-def get_temperature():
-        city = request.form["city"]
+
+
+def get_temperature(city):
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={config.api_key}&units=metric"
         response = requests.get(url)
         data = response.json()
         temperature = data["main"]["temp"]
-        return(city, temperature)
+        return(temperature)
 
 
 if __name__ == "__main__":
